af55fb6dbd3d01e4ed61ad51ee2e783fea93ed1e
Michał Strzelczyk <strzelczyk.michal@yahoo.com>
2018-03-27 18:47:35 +0200

    Add basic js code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 var name = prompt('Enter your name dude!');
-alert("Yo, " + name + "! How you doin'?");
-console.log("Yo, " + name + "! How you doin'?");
+alert("Yo, " + name + "! How you doin'?!");
+console.log("Yo, " + name + "! How you doin'?!");
